d3227787e2ea25f257596cadb43bef0b385667a1
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2022-10-30 00:00:38 +0200

    chore: adiccao de mapa

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([-25.9496182,32.597326], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
