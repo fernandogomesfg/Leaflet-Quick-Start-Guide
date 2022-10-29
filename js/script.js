3fc9faa085a04a0a709012f7e43618846b84308a
Fernando Gomes <59961857+fernandogomesfg@users.noreply.github.com>
2022-10-30 00:22:23 +0200

    chore: Trabalhando com pop-ups

--- js/script.js
+++ js/script.js
@@ -8,15 +8,30 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 var marker = L.marker([-25.9496182,32.597326]).addTo(map);
 
-var circle = L.circle([-25.9496182,32.597326], {
+var circle = L.circle([-25.950588, 32.602551], {
     color: '#ff12aa',
     fillColor: '#1245fa',
     fillOpacity: 0.5,
-    radius: 500
+    radius: 100
 }).addTo(map);
 
 var polygon = L.polygon([
-    [-25.9496182,32.597326],
-    [-25.95036,32.6005769],
-    [-25.950636,32.605769]
+    [-25.952401, 32.603645],
+    [-25.951967, 32.605544],
+    [-25.954938, 32.604461]
 ]).addTo(map);
+
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
